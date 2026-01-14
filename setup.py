bba9fd19e22ff92124bb1060b622099efac6b8a8
TW-RF54732 <127068503+TW-RF54732@users.noreply.github.com>
2026-01-14 21:14:56 +0800

    Bump version to 1.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="onesource-cli",
-    version="1.1.0",
+    version="1.1.2",
     author="lolLeo",
     author_email="your.email@example.com",
     description="A vibe coding tool to aggregate project code for LLMs.",
