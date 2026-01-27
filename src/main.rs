1597c76620dcbf22008975bc739a2bfd448149f8
TW-RF54732 <127068503+TW-RF54732@users.noreply.github.com>
2026-01-27 13:40:24 +0800

    test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use clap::{Parser, builder, error};
+use std::{ path::PathBuf};
+use ignore::{Walk, WalkBuilder};
+
+#[derive(Parser,Debug)]
+#[command(name = "onesource", author = "lolLeo", version = "1.0")]
+struct Args{
+    #[arg(default_value = ".",help = "Set location")]
+    path :PathBuf,
+
+    #[arg(long,action = clap::ArgAction::SetTrue,help="Don't use .gitignore")]
+    no_ignore:bool,
+
+    #[arg(short,long,default_value="allCode")]
+    output_name:String,
+    #[arg(short,long,default_value="txt")]
+    extension:String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    debug_log(&args);
+    let walker = WalkBuilder::new(&args.path)
+        .standard_filters(!args.no_ignore)
+        .build();
+
+    for result in walker{
+        match result {
+            Ok(entry)=>{
+                print!("{:#?}",entry)
+            }
+            Err(error)=>{
+                print!("{}",error)
+            }
+        }
+    }
+}
+
+fn debug_log(args:&Args){
+    println!("======ARGS======");
+    println!("Target path: {:#?}",args);    
+    println!("======Others======")    
 }
